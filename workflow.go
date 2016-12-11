d62af804af60e02cfebf78bd34256da495264330
jspc <james@zero-internet.org.uk>
2016-12-11 13:09:18 +0000

    Generate simple workflow config

---
+++ workflow.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "encoding/json"
+)
+
+type Workflow struct {
+    Name string
+    Steps []Step
+    Variables map[string]string
+}
+
+func ParseWorkflow(data string) (w Workflow, err error) {
+    err = json.Unmarshal([]byte(data), &w)
+
+    return
+}
