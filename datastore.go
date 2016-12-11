a9a222f95790e7372228adf44fb6420eab070509
jspc <james@zero-internet.org.uk>
2016-12-11 13:04:29 +0000

    Load and unload workflow state from redis

--- datastore.go
+++ datastore.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+    "encoding/json"
+    "fmt"
+    "strings"
+
     "gopkg.in/redis.v5"
 )
 
@@ -20,4 +24,49 @@ func NewDatastore(uri string) (d Datastore, err error) {
 
     _,err = d.db.Ping().Result()
     return
+}
+
+func (d Datastore)LoadWorkflow(name string) (wf Workflow, err error) {
+    var config string
+
+    if config, err = d.load( wfConfigName(name) ); err != nil {
+        return
+    }
+
+    return ParseWorkflow(config)
+}
+
+func (d Datastore)LoadWorkflowRunner(uuid string) (wfr WorkflowRunner, err error) {
+    var config string
+
+    if config, err = d.load( wfStateName(uuid) ); err != nil {
+        return
+    }
+
+    return ParseWorkflowRunner(config)
+}
+
+func (d Datastore)DumpWorkflowRunner(wfr WorkflowRunner) error{
+    j, err := json.Marshal(wfr)
+    if err != nil {
+        return err
+    }
+
+    return d.db.Set(wfStateName(wfr.UUID), j, 0).Err()
+}
+
+func (d Datastore)load(key string) (string, error) {
+    return d.db.Get(key).Result()
+}
+
+func normaliseName(wfName string) string {
+    return strings.Replace(wfName, " ", "_", -1)
+}
+
+func wfConfigName(wfName string) string {
+    return fmt.Sprintf("workflow.%s", normaliseName(wfName))
+}
+
+func wfStateName(uuid string) string {
+    return fmt.Sprintf("state.%s", uuid)
 }
