gincorp/gin

commit 5cf2f03612362475280771a3c8d9d15c42d051d4

Handle API data from rabbit

taskmanager/master_manager.go
@@ -42,33 +42,49 @@ func (m MasterManager) Consume(body string) (output map[string]interface{}, err 
 	}
 
 	output = b.(map[string]interface{})
-	uuid := output["UUID"].(string)
-	if wfr, err = m.datastore.LoadWorkflowRunner(uuid); err != nil {
-		return
-	}
 
-	idx, step := wfr.Current()
-	step.SetStatus(output)
-	wfr.Workflow.Steps[idx] = step
+	if output["InitWorkflow"] != nil {
+		req := output["InitWorkflow"].(map[string]interface{})
 
-	switch output["Register"].(type) {
-	case string:
-		register := output["Register"].(string)
-
-		switch output["Data"].(type) {
-		case map[string]interface{}:
-			data := output["Data"].(map[string]interface{})
-			wfr.Variables[register] = data
-
-		default:
-			log.Println("Not registering output: got garbage back")
+		uuid, err = m.Load(output["UUID"].(string), req["Name"].(string), req["Variables"])
+		if err != nil {
+			return
 		}
-	}
 
-	if output["Failed"].(bool) {
-		wfr.Fail(fmt.Sprintf("Step %q failed. See below", wfr.Last))
-		m.datastore.DumpWorkflowRunner(wfr)
-		return
+		if wfr, err = m.datastore.LoadWorkflowRunner(uuid); err != nil {
+			return
+		}
+
+	} else {
+		uuid = output["UUID"].(string)
+		if wfr, err = m.datastore.LoadWorkflowRunner(uuid); err != nil {
+			return
+		}
+
+		idx, step := wfr.Current()
+		step.SetStatus(output)
+		wfr.Workflow.Steps[idx] = step
+
+		switch output["Register"].(type) {
+		case string:
+			register := output["Register"].(string)
+
+			switch output["Data"].(type) {
+			case map[string]interface{}:
+				data := output["Data"].(map[string]interface{})
+				wfr.Variables[register] = data
+
+			default:
+				log.Println("Not registering output: got garbage back")
+			}
+		}
+
+		if output["Failed"].(bool) {
+			wfr.Fail(fmt.Sprintf("Step %q failed. See below", wfr.Last))
+			m.datastore.DumpWorkflowRunner(wfr)
+			return
+		}
+
 	}
 
 	m.datastore.DumpWorkflowRunner(wfr)
@@ -81,9 +97,8 @@ func (m MasterManager) Consume(body string) (output map[string]interface{}, err 
 	return
 }
 
-// Load ...
 // Load a workflow from storage and create a WorkflowRunner state machine
-func (m MasterManager) Load(name string, variables map[string]interface{}) (uuid string, err error) {
+func (m MasterManager) Load(u, name string, variables interface{}) (uuid string, err error) {
 	wf, err := m.datastore.LoadWorkflow(name)
 	if err != nil {
 		return
@@ -103,17 +118,17 @@ func (m MasterManager) Load(name string, variables map[string]interface{}) (uuid
 	return wfr.UUID, nil
 }
 
-// Continue ...
-// Should there be a next step in the workflow, compile step templates
-// and push the step to the emssage queue
-func (m MasterManager) Continue(uuid string) {
+// Continue will, should there be a next step in the workflow, compile step templates
+// and push the step to the emssage queue. Otherwise it'll set the relevant Workflow Runner
+// to completed.
+func (m MasterManager) Continue(uuid string) (step workflow.Step, done bool) {
 	wfr, err := m.datastore.LoadWorkflowRunner(uuid)
 	if err != nil {
 		log.Print(err)
 		return
 	}
 
-	step, done := wfr.Next()
+	step, done = wfr.Next()
 
 	if done {
 		wfr.End()
@@ -128,22 +143,11 @@ func (m MasterManager) Continue(uuid string) {
 
 		step.UUID = wfr.UUID
 
-		j, err := step.JSON()
-		if err != nil {
-			wfr.Fail(fmtError(step, err))
-			m.datastore.DumpWorkflowRunner(wfr)
-
-			return
-		}
-
-		if err := node.Producer.send(j); err != nil {
-			wfr.Fail(fmtError(step, err))
-		}
-
 		wfr.Last = step.Name
-	}
+		m.datastore.DumpWorkflowRunner(wfr)
 
-	m.datastore.DumpWorkflowRunner(wfr)
+	}
+	return step, done
 }
 
 func fmtError(step workflow.Step, err error) string {

commit daa93723907248d58b6b5bc6bdf424959a7bd6c8

Always dump workflow runner.
Commit 5cf2f03612362475280771a3c8d9d15c42d051d4 introduced a regression which meant that on ending a workflow runner nothing was persisted, thus leaving every runner in a 'running' state

taskmanager/master_manager.go
@@ -144,9 +144,9 @@ func (m MasterManager) Continue(uuid string) (step workflow.Step, done bool) {
 		step.UUID = wfr.UUID
 
 		wfr.Last = step.Name
-		m.datastore.DumpWorkflowRunner(wfr)
-
 	}
+
+	m.datastore.DumpWorkflowRunner(wfr)
 	return step, done
 }
 