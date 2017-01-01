3d76407eef9d0ba832fdf41d0711795388c2e5e2
jspc <james@zero-internet.org.uk>
2017-01-01 12:51:30 +0000

    Correct typo

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,13 +2,13 @@
 
 set -axe
 
-if [[ ! -z "${TRAVIS_TAG}" && "${TRAVIS_GO_VERSION}"="1.7.4" ]]; then
+if [ ! -z "${TRAVIS_TAG}" ] && [ "${TRAVIS_GO_VERSION}" = "1.7.4" ]; then
     go get -u github.com/goreleaser/releaser
     releaser
 
-    cp -v dist/gin_Linux_x86_64/gib linux/gin
+    cp -v dist/gin_Linux_x86_64/gin linux/gin
 
-    docker login docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+    docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
     docker build -t gincorp/gin .
     docker push gincorp/gin
 fi
