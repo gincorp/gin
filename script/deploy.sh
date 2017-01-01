0d0d2902dd2228901dbb0cd4f8a888efb8a7bc3a
jspc <james@zero-internet.org.uk>
2017-01-01 13:18:10 +0000

    Push tagged versions

--- script/deploy.sh
+++ script/deploy.sh
@@ -9,6 +9,7 @@ if [ ! -z "${TRAVIS_TAG}" ] && [ "${TRAVIS_GO_VERSION}" = "1.7.4" ]; then
     cp -v dist/gin_Linux_x86_64/gin linux/gin
 
     docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+    docker build -t gincorp/gin:${TRAVIS_TAG} .
     docker build -t gincorp/gin .
     docker push gincorp/gin
 fi
